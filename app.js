6ad5bd3c49649c62c8af366bf66b45529d64c450
joji000 <172300802+joji000@users.noreply.github.com>
2024-07-04 19:15:34 +0700

    finished

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ function populateBoard(size) {
   squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
   board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-  document.querySelector(".error").style.display = "none";
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
